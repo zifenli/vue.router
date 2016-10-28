46ff6c28daec38b55427811dc36534fe0ef99f20
zifenli <zifenli@gmail.com>
2016-10-28 14:16:54 +0800

    添加大文件

---
+++ src/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by lizifen on 16/7/11.
+ */
+import Vue from 'vue'
+import App from './app.vue'
+
+new Vue({
+    el: 'body',
+    data: {
+        message: "Hello Vue"
+    },
+    components: { App }
+})
